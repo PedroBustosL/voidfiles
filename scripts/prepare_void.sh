ca7ac46628471ea18099e19b1957f6ef049cba25
kyozons <bustoslatorre@gmail.com>
2022-11-19 21:22:39 -0300

    first commit

---
+++ scripts/prepare_void.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+########## PKG MAGNAMENT ############
+sudo xbps-install -Suv
+PACKAGES=$HOME/voidfiles/pkgs.txt
+
+for PKG in $PACKAGES
+do
+	sudo xpbs-install -y $PKG
+done
+
+######### CONFIG MAGNAMENT #########
+cp $HOME/voidfiles/herbswm/* $HOME/.config/herbstluftwm/
+sudo cp $HOME/voidfiles/scripts/scratchpad /usr/local/bin/
+sudo cmod +x /usr/local/bin/scratchpad
+
+
+####### SETUP VOID PKGS ###########
+mkdir $HOME/pkgs
+https://github.com/void-linux/void-packages.git
