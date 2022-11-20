8ccb89e54e773ef65d9fbf0fa1a221c93937c98c
kyozons <bustoslatorre@gmail.com>
2022-11-19 21:59:26 -0300

    lightdm conf

--- scripts/prepare_void.sh
+++ scripts/prepare_void.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-########## PKG MAGNAMENT ############
+########## PKG MAGNAMENT ########
 sudo xbps-install -Suv
 PACKAGES=$HOME/voidfiles/pkgs.txt
 
@@ -9,12 +9,16 @@ do
 	sudo xpbs-install -y $PKG
 done
 
-######### CONFIG MAGNAMENT #########
+########## CONFIG MAGNAMENT #############################
 cp $HOME/voidfiles/herbswm/* $HOME/.config/herbstluftwm/
 sudo cp $HOME/voidfiles/scripts/scratchpad /usr/local/bin/
 sudo cmod +x /usr/local/bin/scratchpad
 
 
-####### SETUP VOID PKGS ###########
+########## SETUP VOID PKGS ####################
 mkdir $HOME/pkgs
 https://github.com/void-linux/void-packages.git
+
+########## SETUP LIGHTDM ################
+cp $HOME/voidfiles/lightdm/* /etc/lightdm/
+sed -i "s/PLACEHOLDER/$(whoami)/" /etc/lightdm/lightdm-mini-greeter.conf
