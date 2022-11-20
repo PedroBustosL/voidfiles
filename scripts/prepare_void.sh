78c6aac666976a51e8563cd1629fde54181fed01
kyozons <bustoslatorre@gmail.com>
2022-11-20 00:00:05 -0300

    Corrected sv errors, syntax and sudo

--- scripts/prepare_void.sh
+++ scripts/prepare_void.sh
@@ -2,23 +2,35 @@
 
 ########## PKG MAGNAMENT ########
 sudo xbps-install -Suv
-PACKAGES=$HOME/voidfiles/pkgs.txt
+PACKAGES=$(cat $HOME/voidfiles/pkgs.txt)
 
 for PKG in $PACKAGES
 do
-	sudo xpbs-install -y $PKG
+	sudo xbps-install -y $PKG
 done
 
+########## START SERVICES #############
+sudo ln -s /etc/sv/lightdm /var/service
+sudo ln -s /etc/sv/elogind /var/service
+sudo ln -s /etc/sv/dbus /var/service
+sudo sv up lightdm
+sudo sv up elogind 
+sudo sv up dbus 
+
 ########## CONFIG MAGNAMENT #############################
+mkdir .config .config/herbstluftwm
 cp $HOME/voidfiles/herbswm/* $HOME/.config/herbstluftwm/
 sudo cp $HOME/voidfiles/scripts/scratchpad /usr/local/bin/
-sudo cmod +x /usr/local/bin/scratchpad
+sudo chmod +x /usr/local/bin/scratchpad
+cp $HOME/voidfiles/.bashrc $HOME/.bashrc
+source $HOME/.bashrc
 
 
 ########## SETUP VOID PKGS ####################
 mkdir $HOME/pkgs
-https://github.com/void-linux/void-packages.git
+git clone https://github.com/void-linux/void-packages.git $HOME/pkgs
 
-########## SETUP LIGHTDM ################
-cp $HOME/voidfiles/lightdm/* /etc/lightdm/
-sed -i "s/PLACEHOLDER/$(whoami)/" /etc/lightdm/lightdm-mini-greeter.conf
+########## SETUP LIGHTDM ####################################################
+sudo cp $HOME/voidfiles/lightdm/* /etc/lightdm/
+sudo sed -i "s/PLACEHOLDER/$(whoami)/" /etc/lightdm/lightdm-mini-greeter.conf
+
