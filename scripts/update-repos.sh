145943dcad0a743fd0d5c3a66a91edc7825b2565
Kyozons <bustoslatorre@gmail.com>
2022-12-12 16:42:19 -0300

    autostart updt and polybar + scripts

--- scripts/update-repos.sh
+++ scripts/update-repos.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [[ -d $HOME/voidfiles ]];
+if [ -d $HOME/voidfiles ];
 then
-	cd $HOME/voidfiles || git pull
-elseif [[ -d $HOME/soportetimundo ]]
-then
-	cd $HOME/soportetimundo || git pull
+	cd $HOME/voidfiles || exit
+	git pull > /tmp/git_pull.log &
 else
-	echo "No se encuentra directorio" > /tmp/git_pull.log
+	echo "No se encuentra directorio "$HOME/voidfiles" " > /tmp/git_pull.log
+fi
+if [ -d $HOME/soportetimundo ];
+then
+	cd $HOME/soportetimundo || exit 
+	git pull > /tmp/git_pull.log &
+else
+	echo "No se encuentra directorio "$HOME/soportetimundo" " > /tmp/git_pull.log
+fi
